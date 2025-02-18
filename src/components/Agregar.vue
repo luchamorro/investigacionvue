7a2c283398697d8a23e957d06d9f18972db7d4b9
luChamorro <chamorro.lucia@gmail.com>
2025-02-18 22:15:19 +0100

    comentarios en agregar

--- src/components/Agregar.vue
+++ src/components/Agregar.vue
@@ -1,7 +1,8 @@
 <script setup>
 
 import { ref } from 'vue';
-//array reactivo para almacenar los cuadraditos
+
+//array reactivo para almacenar  los cuadrados
 const littleSquares = ref([]);
 
 const addSquares = () => {
@@ -10,9 +11,7 @@ const addSquares = () => {
   const classes = ["littleSquare", "littleSquare2", "littleSquare3", "littleSquare4", "littleSquare5", "littleSquare6" ];
   //Seleccionar una clase aleatoria
   const randomClass = classes[Math.floor(Math.random()*classes.length)];
- 
-  littleSquares.value.push({class: randomClass}); //añadir nuevo cuadrado al array
-
+  littleSquares.value.push({nameClass: randomClass}); //añadir nuevo cuadrado al array
 };
 
 </script>
@@ -21,14 +20,12 @@ const addSquares = () => {
   <div class="example">
     <h2>Agregar</h2>
   <br>
-    
-   <div class="content">  
-     
-    <button class="buttons" @click="addSquares" >+</button>
-  
-  
+  <div class="content">  
+  <!-- Ejemplo de @click -->
+  <button class="buttons" @click="addSquares" >+</button>
     <div class="squaresBox">
-    <div v-for = "(square,index) in littleSquares" :key="index" :class="square.class">
+      <!-- Ejemplo de v-for y v-bind -->
+    <div v-for = "(square,index) in littleSquares" :key="index" :class="square.nameClass">
       </div>
     </div>
   </div>
